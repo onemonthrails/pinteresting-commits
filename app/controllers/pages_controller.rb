4e38501ef391749fb48f08499139f16ee7cdf2ac
Chris Castiglione <castig@gmail.com>
2013-09-18 00:16:56 -0400

    Creating more pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def about 
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Pinteresting::Application.routes.draw do
   root "pages#home"
+  get "about" => "pages#about"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
