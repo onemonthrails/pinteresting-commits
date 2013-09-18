c786c41745cef9e82d83e5d1e9d48c3c81cb27d0
Chris Castiglione <castig@gmail.com>
2013-09-17 23:54:52 -0400

    rails generate controller pages home AND change home.html.erb text

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Pinteresting::Application.routes.draw do
+  get "pages/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
