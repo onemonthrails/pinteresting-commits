4fb60289366f0a21194f015bfce14d7ca50ac76d
Chris Castiglione <castig@gmail.com>
2013-09-21 22:09:24 -0400

    Devise Step 1 Completed: Ensure you have defined default url options in your environments files.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  # Required for Heroku
+  config.action_mailer.default_url_options = { :host => 'http://pinteresting-commits.herokuapp.com/' }
 end
