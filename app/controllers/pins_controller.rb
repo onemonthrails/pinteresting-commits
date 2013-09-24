c13c82d667565e78ecf1a04f77cdb35cb282b0b5
Chris Castiglione <castig@gmail.com>
2013-09-24 10:30:52 -0400

    Add migration for :user_id column, rake db:migrate to update the database, add current_user.pins.build when creating new Pins so that they are associated with (or owned by) a user

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -9,32 +9,32 @@ class PinsController < ApplicationController
   end
 
   def new
-    @pin = Pin.new
+    @pin = current_user.pins.build
   end
 
   def edit
   end
 
   def create
-    @pin = Pin.new(pin_params)
+    @pin = current_user.pins.build(pin_params)
     if @pin.save
-      redirect_to @pin, notice: 'Pin was successfully created.' 
+      redirect_to @pin, notice: 'Pin was successfully created.'
     else
-      render action: 'new' 
+      render action: 'new'
     end
   end
 
   def update
     if @pin.update(pin_params)
-      redirect_to @pin, notice: 'Pin was successfully updated.' 
+      redirect_to @pin, notice: 'Pin was successfully updated.'
     else
-      render action: 'edit' 
+      render action: 'edit'
     end
   end
 
   def destroy
     @pin.destroy
-    redirect_to pins_url 
+    redirect_to pins_url
   end
 
   private
@@ -43,8 +43,13 @@ class PinsController < ApplicationController
       @pin = Pin.find(params[:id])
     end
 
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
+    end
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
-      params.require(:pin).permit(:description)
+      params.require(:pin).permit(:description, :image)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130922043841) do
+ActiveRecord::Schema.define(version: 20130923041812) do
 
   create_table "pins", force: true do |t|
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "pins", ["user_id"], name: "index_pins_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
