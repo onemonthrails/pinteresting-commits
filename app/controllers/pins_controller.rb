3fccd7bdb54de0eb2fec5233904e76c1944760c0
Chris Castiglione <castig@gmail.com>
2013-09-24 12:04:02 -0400

    update pins_controller to remove correct_user method (it was too early and out of order)

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -43,11 +43,6 @@ class PinsController < ApplicationController
       @pin = Pin.find(params[:id])
     end
 
-    def correct_user
-      @pin = current_user.pins.find_by(id: params[:id])
-      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
-    end
-
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
       params.require(:pin).permit(:description, :image)
